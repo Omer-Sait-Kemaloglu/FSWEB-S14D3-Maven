22af7c813b86caeca1102d60a6f38dd4b34b3ee0
Omer-Sait-Kemaloglu <omersaitkemaloglu@gmail.com>
2025-01-15 19:21:24 +0300

    ford

--- src/main/java/org/example/company/Ford.java
+++ src/main/java/org/example/company/Ford.java
@@ -1,4 +1,24 @@
 package org.example.company;
 
-public class Ford {
+public class Ford extends Car{
+    public Ford(int cylinders, String name){
+        super(cylinders, name);
+    }
+
+    @Override
+
+    public void startEngine(){
+        System.out.println(getClass().getSimpleName() + ":  the Ford's engine is starting.");
+    }
+
+    @Override
+
+    public void accelerate(){
+        System.out.println(getClass().getSimpleName() + ":  the Ford is accelerating.");
+    }
+
+    @Override
+    public void brake(){
+        System.out.println(getClass().getSimpleName() + ": the Ford is braking.");
+    }
 }
