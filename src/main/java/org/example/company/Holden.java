3c24ff716c8814c4cad26ad2257a39871554594b
Omer-Sait-Kemaloglu <omersaitkemaloglu@gmail.com>
2025-01-15 19:16:00 +0300

    holden

--- src/main/java/org/example/company/Holden.java
+++ src/main/java/org/example/company/Holden.java
@@ -1,4 +1,22 @@
 package org.example.company;
 
-public class Holden {
+public class Holden extends Car {
+    public Holden(int cylinders, String name){
+        super(cylinders, name);
+    }
+
+    @Override
+    public void startEngine(){
+        System.out.println(getClass().getSimpleName() + ":  the Holden's engine is starting.");
+    }
+
+    @Override
+    public void accelerate(){
+        System.out.println(getClass().getSimpleName() + ": the Holden is accelerating.");
+    }
+
+    @Override
+    public void brake(){
+        System.out.println(getClass().getSimpleName() + ": the Holden is braking.");
+    }
 }
